7d26f1d87c11bc277df8b612024f11dea82b8208
Kyle Bartush <kbartush@kochava.com>
2019-11-04 14:48:37 -0800

    feat(update): updating mazzaroth xdr to latest version
    - Removed schema because we're no longer using it
    - Updated block header to include transactionHeight and
    consensusSequenceNumber

--- test/client/node-client.js
+++ test/client/node-client.js
@@ -28,10 +28,7 @@ describe('node client test', () => {
     category: {
       enum: 2,
       value: {
-        contract: base64,
-        schema: {
-          tables: []
-        }
+        contract: base64
       }
     }
   }
@@ -139,10 +136,7 @@ describe('node client test', () => {
         category: {
           enum: 2,
           value: {
-            contract: base64,
-            schema: {
-              tables: []
-            }
+            contract: base64
           }
         }
       }
@@ -189,10 +183,7 @@ describe('node client test', () => {
         category: {
           enum: 2,
           value: {
-            contract: base64,
-            schema: {
-              tables: []
-            }
+            contract: base64
           }
         }
       }
@@ -216,10 +207,7 @@ describe('node client test', () => {
         category: {
           enum: 2,
           value: {
-            contract: base64,
-            schema: {
-              tables: []
-            }
+            contract: base64
           }
         }
       }
