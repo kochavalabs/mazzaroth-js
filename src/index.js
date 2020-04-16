bffa66eb4d604449278e9bd9ec2133162c1b3d3a
Kyle Bartush <kbartush@kochava.com>
2020-04-16 15:38:28 -0700

    feat(address): export function that can get address from priv key

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import NodeClient from './client/node-client.js'
 import ContractClient from './client/contract-client.js'
-import { ReceiptSubscriptionResult } from 'mazzaroth-xdr'
-import { RunExecutionPlan, BuildReceiptSubscription, XDRtoJSON, JSONtoXDR } from './client/utils.js'
+import { RunExecutionPlan } from './client/utils.js'
 import { fromPrivate, generateKeys } from './crypto/ecc-ed25519.js'
-
-let Socket = null
-
-if (typeof WebSocket === 'undefined') {
-  Socket = require('ws')
-} else {
-  Socket = WebSocket
-}
 
 function AccountGenerate () {
   return generateKeys()
@@ -20,26 +11,4 @@ function AddressFromPrivate (priv) {
   return fromPrivate(priv)
 }
 
-function ReceiptSubscribe (host, subscriptionObject, onMessage) {
-  const ws = new Socket(`ws://${host}/subscribe/receipt`)
-  ws.onopen = function () {
-    const subscription = BuildReceiptSubscription(subscriptionObject)
-    ws.send(subscription.toXDR('base64'))
-  }
-  ws.onmessage = function (e) {
-    const result = ReceiptSubscriptionResult()
-    result.fromXDR(e.data, 'base64')
-    onMessage(result.toJSON())
-  }
-}
-
-export {
-  NodeClient,
-  ContractClient,
-  AccountGenerate,
-  RunExecutionPlan,
-  AddressFromPrivate,
-  ReceiptSubscribe,
-  XDRtoJSON,
-  JSONtoXDR
-}
+export { NodeClient, ContractClient, AccountGenerate, RunExecutionPlan, AddressFromPrivate }
